d0da27c7b348aa2dc9d24dfbf0d0cee89f7dd5cd
kaizsv <kai.zsv@gmail.com>
2016-06-12 20:03:55 +0800

    add exit handler

--- fall_detection.py
+++ fall_detection.py
@@ -21,8 +21,18 @@ with Manager() as manager:
     if not is_ok:
         c.open()
 
+    def exit_handler():
+        is_ok = c.write_single_register(0, 0)
+        if not is_ok:
+            c.open()
+
+    # registing exit handler
+    atexit.register(exit_handler)
+
     # data sampling period
     sleep_time = 0.00833
+    interval = int(1 / sleep_time)
+	i = 0
 
     # consitent define in paper
     T_alpha_a = 2.0
@@ -51,6 +61,11 @@ with Manager() as manager:
     acc_scale_2 = [8192/8210, 8192/8315, 8192/8172.5]
     _4g = 0.0001220703125
 
+    alpha_a_int = []
+    alpha_b_int = []
+    omg_a_int = []
+    omg_b_int = []
+
     # share data for multiprocessing
     data_1 = Array('i', 6)
     data_2 = Array('i', 6)
@@ -66,102 +81,100 @@ with Manager() as manager:
     mpu2.start()
 
     while True:
-        # holding data during time interval(1s)
-        alpha_a_int = []
-        alpha_b_int = []
-        omg_a_int = []
-        omg_b_int = []
-        # get data from imu 10 time in 1s
-        interval = int(1 / sleep_time)
-        for _ in xrange(interval):
-            acc_1 = [data_1[0]-acc_offset_1[0], data_1[1]-acc_offset_1[1], data_1[2]-acc_offset_1[2]]
-            acc_2 = [data_2[0]-acc_offset_2[0], data_2[1]-acc_offset_2[1], data_2[2]-acc_offset_2[2]]
-            gyro_1 = [data_1[3], data_1[4], data_1[5]]
-            gyro_2 = [data_2[3], data_2[4], data_2[5]]
 
-            # skip noise data
-            if max(gyro_1) > 600 or min(gyro_1) < -600 or max(gyro_2) > 600 or min(gyro_2) < -600:
-                time.sleep(sleep_time)
-                continue
+        acc_1 = [data_1[0]-acc_offset_1[0], data_1[1]-acc_offset_1[1], data_1[2]-acc_offset_1[2]]
+        acc_2 = [data_2[0]-acc_offset_2[0], data_2[1]-acc_offset_2[1], data_2[2]-acc_offset_2[2]]
+        gyro_1 = [data_1[3], data_1[4], data_1[5]]
+        gyro_2 = [data_2[3], data_2[4], data_2[5]]
 
-            # scale acc data
-            for i in xrange(3):
-                acc_1[i] = acc_1[i] * _4g * acc_scale_1[i]
-                acc_2[i] = acc_2[i] * _4g * acc_scale_2[i]
-            #print(acc_1, acc_2, gyro_1, gyro_2)
-            alpha_a_int.append(sqrt(pow(acc_1[0],2) + pow(acc_1[1],2) + pow(acc_1[2],2)))
-            alpha_b_int.append(sqrt(pow(acc_2[0],2) + pow(acc_2[1],2) + pow(acc_2[2],2)))
-            omg_a_int.append(sqrt(pow(gyro_1[0],2) + pow(gyro_1[1],2) + pow(gyro_1[2],2)))
-            omg_b_int.append(sqrt(pow(gyro_2[0],2) + pow(gyro_2[1],2) + pow(gyro_2[2],2)))
-            time.sleep(sleep_time)           
+        # skip noise data
+        if max(gyro_1) > 600 or min(gyro_1) < -600 or max(gyro_2) > 600 or min(gyro_2) < -600:
+            time.sleep(sleep_time)
+            continue
+
+        # scale acc data
+        for j in xrange(3):
+            acc_1[j] = acc_1[j] * _4g * acc_scale_1[j]
+            acc_2[j] = acc_2[j] * _4g * acc_scale_2[j]
+        #print(acc_1, acc_2, gyro_1, gyro_2)
+        alpha_a_int[i] = sqrt(pow(acc_1[0],2) + pow(acc_1[1],2) + pow(acc_1[2],2))
+        alpha_b_int[i] = sqrt(pow(acc_2[0],2) + pow(acc_2[1],2) + pow(acc_2[2],2))
+        omg_a_int[i] = sqrt(pow(gyro_1[0],2) + pow(gyro_1[1],2) + pow(gyro_1[2],2))
+        omg_b_int[i] = sqrt(pow(gyro_2[0],2) + pow(gyro_2[1],2) + pow(gyro_2[2],2))
+
+        """
+            Monitor if people are static or dynamic during the
+            present time segment.
+        """
+        if (max(alpha_a_int) - min(alpha_a_int) < 0.45 and max(alpha_b_int) - min(alpha_b_int) < 0.45 
+                and max(omg_a_int) - min(omg_a_int) < 70 and max(omg_b_int) - min(omg_b_int) < 70):
+            # static
+            is_ok = c.write_single_register(1, 0)
+            if not is_ok:
+                c.open()
 
             """
-                Monitor if people are static or dynamic during the
-                present time segment.
+                Recognize the present static posture: is it lying?
             """
-            if (max(alpha_a_int) - min(alpha_a_int) < 0.45 and max(alpha_b_int) - min(alpha_b_int) < 0.45 
-                    and max(omg_a_int) - min(omg_a_int) < 70 and max(omg_b_int) - min(omg_b_int) < 70):
-                # static
-                is_ok = c.write_single_register(1, 0)
+            acc_1[0] = max(-1, min(1, acc_1[0]))
+            acc_2[0] = max(-1, min(1, acc_2[0]))
+            if acos(acc_1[0]) > 0.61 and acos(acc_2[0]) > 0.61:
+                is_ok = c.write_single_register(3, 1)
+                if not is_ok:
+                    c.open()
+                """
+                    Determine if the transition before the present
+                    lying posture is intentional.
+                """
+                if (max(alpha_a_int) > T_alpha_a or max(alpha_b_int) > T_alpha_b 
+                        or max(omg_a_int) > T_omg_a or max(omg_b_int) > T_omg_b):
+                    # fall
+                    is_ok = c.write_single_register(2, 1)
+                    if not is_ok:
+                        c.open()
+                    time.sleep(5)
+                    # reset fall
+                    is_ok = c.write_single_register(2, 0)
+                    if not is_ok:
+                        c.open()
+            else:
+                is_ok = c.write_single_register(3, 0)
                 if not is_ok:
                     c.open()
 
-                """
-                    Recognize the present static posture: is it lying?
-                """
-                acc_1[0] = max(-1, min(1, acc_1[0]))
-                acc_2[0] = max(-1, min(1, acc_2[0]))
-                if acos(acc_1[0]) > 0.61 and acos(acc_2[0]) > 0.61:
-                    is_ok = c.write_single_register(3, 1)
-                    if not is_ok:
-                        c.open()
-                    """
-                        Determine if the transition before the present
-                        lying posture is intentional.
-                    """
-                    if (max(alpha_a_int) > T_alpha_a or max(alpha_b_int) > T_alpha_b 
-                            or max(omg_a_int) > T_omg_a or max(omg_b_int) > T_omg_b):
-                        # fall
-                        is_ok = c.write_single_register(2, 1)
-                        if not is_ok:
-                            c.open()
-                        time.sleep(5)
-                        # reset fall
-                        is_ok = c.write_single_register(2, 0)
-                        if not is_ok:
-                            c.open()
-                else:
-                    is_ok = c.write_single_register(3, 0)
-                    if not is_ok:
-                        c.open()
 
+        else:
+            # dynamic
+            is_ok = c.write_single_register(1, 1)
+            if not is_ok:
+               c.open()
 
-            else:
-                # dynamic
-                is_ok = c.write_single_register(1, 1)
-                if not is_ok:
-                   c.open()
+        i += 1
+        i %= interval
+        time.sleep(sleep_time)
 
-            f = open('C.txt', 'a')
-            str1 = ''.join(str(alpha_a_int[-1]))
-            f.write(str1)
-            f.write(' ')
-            str1 = ''.join(str(alpha_b_int[-1]))
-            f.write(str1)
-            f.write(' ')
-            str1 = ''.join(str(omg_a_int[-1]))
-            f.write(str1)
-            f.write(' ')
-            str1 = ''.join(str(omg_b_int[-1]))
-            f.write(str1)
-            f.write(' ')
-            str1 = ''.join(str(c.read_holding_registers(1, 1)))
-            f.write(str1)
-            f.write(' ')
-            str1 = ''.join(str(c.read_holding_registers(3, 1)))
-            f.write(str1)
-            f.write(' ')
-            str1 = ''.join(str(c.read_holding_registers(2, 1)))
-            f.write(str1)
-            f.write('\n')
-            f.close()
+        """
+        f = open('C.txt', 'a')
+        str1 = ''.join(str(alpha_a_int[-1]))
+        f.write(str1)
+        f.write(' ')
+        str1 = ''.join(str(alpha_b_int[-1]))
+        f.write(str1)
+        f.write(' ')
+        str1 = ''.join(str(omg_a_int[-1]))
+        f.write(str1)
+        f.write(' ')
+        str1 = ''.join(str(omg_b_int[-1]))
+        f.write(str1)
+        f.write(' ')
+        str1 = ''.join(str(c.read_holding_registers(1, 1)))
+        f.write(str1)
+        f.write(' ')
+        str1 = ''.join(str(c.read_holding_registers(3, 1)))
+        f.write(str1)
+        f.write(' ')
+        str1 = ''.join(str(c.read_holding_registers(2, 1)))
+        f.write(str1)
+        f.write('\n')
+        f.close()
+        """
